aa7c0a9fc15fddc499467a3a4a2e5a9a73f63738
rizky <rizky@Rizky>
2024-02-12 17:30:21 +0700

    Update Test Suite

---
+++ Test Suites/Test Suite 1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>73941b20-19c5-4723-a240-b7dad1f23777</testSuiteGuid>
+</TestSuiteEntity>
